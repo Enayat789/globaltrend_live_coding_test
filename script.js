229158ec21c6e3e9c80d83007ea908d8f7c9ad8f
enayat789 <enayatbhai2006@gmail.com>
2024-07-26 21:22:05 +0530

    improved

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const toggleHemburger = document.querySelector("#toggleHamburger");
+const navLinks = document.getElementById("navLinks");
+
+let checkNav = false;
 
 toggleHemburger.onclick = () => {
+  if (checkNav) {
+    navLinks.style.display = "none";
+    checkNav = false;
+  } else {
+    navLinks.style.display = "flex";
+    checkNav = true;
+  }
   toggleHemburger.classList.toggle("hamburger-toggle");
 };
